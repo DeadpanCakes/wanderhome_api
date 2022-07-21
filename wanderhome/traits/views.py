75cc7eda718817193b14013d252b0140877f0b10
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-21 16:44:01 -0700

    made view for Traits

--- wanderhome/traits/views.py
+++ wanderhome/traits/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Trait
+from rest_framework import viewsets
+from wanderhome.traits.serializers import TraitsSerializer
 
 # Create your views here.
+class TraitViewSet(viewsets.ModelViewSet):
+    queryset = Trait.objects.all()
+    serializer_class = TraitsSerializer
