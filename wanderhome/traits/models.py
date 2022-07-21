a9111dc2c2eeca887bf1603789791cb2a1a4373d
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-21 16:39:35 -0700

    made basic Trait class

--- wanderhome/traits/models.py
+++ wanderhome/traits/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Trait(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField()
+    options = models.JSONField()
+    def __str__ (self): 
+        return self.name
