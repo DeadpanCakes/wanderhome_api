21a945b34b587e90f030ffa283f4788caccc5763
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 15:22:24 -0700

    registered move model

--- wanderhome/traits/urls.py
+++ wanderhome/traits/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r"traits", views.TraitViewSet)
+router.register(r"moves", views.MoveViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
