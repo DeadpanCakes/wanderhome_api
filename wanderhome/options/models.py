660bddce255d6b5df294c0f933db7f8e641a42df
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 14:21:08 -0700

    made Option class

--- wanderhome/options/models.py
+++ wanderhome/options/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Option(models.Model):
+    text = models.CharField(max_length=100)
+    non_magic_text = models.CharField(max_length=100, null=True, blank=True)
+    non_traumatized_text = models.CharField(
+        max_length=100, null=True, blank=True)
+    non_traumatized_or_magic_text = models.CharField(
+        max_length=100, null=True, blank=True)
+    is_magical = models.BooleanField(default=False)
+    is_traumatized = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
