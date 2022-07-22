4282b7730f5eb822bca2ab6f9fe0a8472c287c99
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-21 18:11:12 -0700

    added max_length

--- wanderhome/traits/models.py
+++ wanderhome/traits/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+app_name = "traits"
 class Trait(models.Model):
     name = models.CharField(max_length=20)
-    description = models.CharField()
+    description = models.CharField(max_length=200)
     options = models.JSONField()
     def __str__ (self): 
         return self.name
