e31b1d625558584c8c4542a63b35e27a4848f2e8
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 16:00:51 -0700

    made Animal class

--- wanderhome/playbooks/models.py
+++ wanderhome/playbooks/models.py
@@ -1,10 +1,17 @@
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, ForeignKey, CASCADE
 
 # Create your models here.
 
 class Playbook(Model):
     name = CharField(max_length=25)
     desc = CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Animal(Model):
+    name = CharField(max_length=25)
+    playbook = ForeignKey(Playbook, on_delete=CASCADE)
 
     def __str__(self):
         return self.name
