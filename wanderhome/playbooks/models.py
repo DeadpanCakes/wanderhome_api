bbdf3a8b2ef03eeb73d2a0d11dd1ffbd3e48c8e4
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 16:04:00 -0700

    made personality model

--- wanderhome/playbooks/models.py
+++ wanderhome/playbooks/models.py
@@ -2,14 +2,25 @@ from django.db.models import Model, CharField, ForeignKey, CASCADE
 
 # Create your models here.
 
+
 class Playbook(Model):
     name = CharField(max_length=25)
     desc = CharField(max_length=200)
+    personality_prompt = CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
 class Animal(Model):
+    name = CharField(max_length=25)
+    playbook = ForeignKey(Playbook, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Personality(Model):
     name = CharField(max_length=25)
     playbook = ForeignKey(Playbook, on_delete=CASCADE)
 
