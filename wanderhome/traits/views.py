2beae849f0e26fdf0f2505870392c6fe66c9399c
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 15:22:33 -0700

    added move view

--- wanderhome/traits/views.py
+++ wanderhome/traits/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from . import models
+from .models import Trait, Move
 from rest_framework import viewsets
-from traits.serializers import TraitsSerializer
+from traits.serializers import MovesSerializer, TraitsSerializer
 
 # Create your views here.
 class TraitViewSet(viewsets.ModelViewSet):
-    queryset = models.Trait.objects.all()
+    queryset = Trait.objects.prefetch_related("move_set")
     serializer_class = TraitsSerializer
+
+class MoveViewSet(viewsets.ModelViewSet):
+    queryset = Move.objects.all()
+    serializer_class = MovesSerializer
