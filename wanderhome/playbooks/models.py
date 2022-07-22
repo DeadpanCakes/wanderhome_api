1338d94177f3bf404b925b63cf734b62e50a1aba
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 15:57:56 -0700

    made Playbook model

--- wanderhome/playbooks/models.py
+++ wanderhome/playbooks/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db.models import Model, CharField
 
 # Create your models here.
+
+class Playbook(Model):
+    name = CharField(max_length=25)
+    desc = CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
