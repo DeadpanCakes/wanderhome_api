7265fd28165a598e3591809239b58b85893138d0
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 15:22:14 -0700

    made serializer for moves

--- wanderhome/traits/serializers.py
+++ wanderhome/traits/serializers.py
@@ -1,7 +1,15 @@
-from . import models
+from .models import Trait, Move
 from rest_framework import serializers
+
 
 class TraitsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = models.Trait
-        fields = ["url", "name", "description", "options"]
+        model = Trait
+        fields = ["url", "name", "description", "move_set"]
+
+
+class MovesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Move
+        fields = ["url", "text", "non_traumatized_text", "non_magic_text",
+                  "non_traumatized_or_magic_text", "is_magical", "is_traumatized"]
