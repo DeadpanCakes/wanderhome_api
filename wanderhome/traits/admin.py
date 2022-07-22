c2d0ca4c32708305e2e902be5c3a8541946df80a
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 15:52:37 -0700

    registered trait models to admin site

--- wanderhome/traits/admin.py
+++ wanderhome/traits/admin.py
@@ -1,3 +1,6 @@
+from binascii import a2b_hex
 from django.contrib import admin
+from traits.models import Category, Trait, Move
 
 # Register your models here.
+admin.site.register([Category, Trait, Move])
