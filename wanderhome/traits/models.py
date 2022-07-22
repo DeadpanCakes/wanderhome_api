d574191a9c087c12b090746cee8fba34b17dc89a
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 15:33:24 -0700

    made category model in traits and migrated

--- wanderhome/traits/models.py
+++ wanderhome/traits/models.py
@@ -1,14 +1,28 @@
+from unicodedata import category
 from django.db import models
 from options.models import Option
 # Create your models here.
 
 app_name = "traits"
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    is_traumatized = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
 class Trait(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
-    def __str__ (self): 
+    is_magic = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
         return self.name
+
 
 class Move(Option):
     trait = models.ForeignKey(Trait, on_delete=models.CASCADE)
