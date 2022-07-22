2944f226689c9a10398ade4298af9730fffd3dac
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 15:21:34 -0700

    made the move class

--- wanderhome/traits/models.py
+++ wanderhome/traits/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-
+from options.models import Option
 # Create your models here.
 
 app_name = "traits"
+
 class Trait(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=200)
-    options = models.JSONField()
     def __str__ (self): 
         return self.name
+
+class Move(Option):
+    trait = models.ForeignKey(Trait, on_delete=models.CASCADE)
