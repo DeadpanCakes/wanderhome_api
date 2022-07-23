9d564e59627527df8940dc4d7566722b295b1bdf
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 18:42:34 -0700

    registered all models

--- wanderhome/playbooks/admin.py
+++ wanderhome/playbooks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Playbook, Animal, Personality, PersonalityOption, Appearance, History, HistoryOption,Relationship, SignatureMove, SeasonalMove
+
 # Register your models here.
+
+admin.site.register([Playbook, Animal, Personality, PersonalityOption, Appearance,
+                    History, HistoryOption,  Relationship, SignatureMove, SeasonalMove])
