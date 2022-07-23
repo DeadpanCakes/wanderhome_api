4f4fbabcc9743b74c0de53fe22006bb88d64d065
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 23:01:52 -0700

    added serializers to views

--- wanderhome/seasons/views.py
+++ wanderhome/seasons/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from . import models, serializers
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
+
+
+class SeasonViewSet(ModelViewSet):
+    queryset = models.Season.objects.all()
+    serializer_class = serializers.SeasonsSerializer
+
+
+class MonthViewSet(ModelViewSet):
+    queryset = models.Month.objects.all()
+    serializer_class = serializers.MonthsSerializer
+
+
+class LackViewSet(ModelViewSet):
+    queryset = models.Lack.objects.all()
+    serializer_class = serializers.LacksSerializer
+
+
+class SignViewSet(ModelViewSet):
+    queryset = models.Sign.objects.all()
+    serializer_class = serializers.SignsSerializer
+
+
+class EventViewSet(ModelViewSet):
+    queryset = models.Event.objects.all()
+    serializer_class = serializers.EffectsSerializer
+
+
+class EffectViewSet(ModelViewSet):
+    queryset = models.Effect.objects.all()
+    serializer_class = serializers.EffectsSerializer
+
+
+class EffectMoveViewSet(ModelViewSet):
+    queryset = models.EffectMove.objects.all()
+    serializer_class = serializers.EffectMovesSerializer
+
+
+class HolidayViewSet(ModelViewSet):
+    queryset = models.Holiday.objects.all()
+    serializer_class = serializers.HolidaysSerializer
+
+
+class TraditionViewSet(ModelViewSet):
+    queryset = models.Tradition.objects.all()
+    serializer_class = serializers.TraditionsSerializer
+
+
+class HolidayMoveViewSet(ModelViewSet):
+    queryset = models.HolidayMove.objects.all()
+    serializer_class = serializers.HolidayMovesSerializer
+
+
+class CustomViewSet(ModelViewSet):
+    queryset = models.Custom.objects.all()
+    serializer_class = serializers.CustomsSerializer
