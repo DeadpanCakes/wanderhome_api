aa88f982ee219daeebad9dc510ad3a2e6aa4c727
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 20:33:01 -0700

    added views to router

---
+++ wanderhome/natures/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r"categories", views.CategoryViewSet)
+router.register(r"natures", views.NatureViewSet)
+router.register(r"aesthetics", views.AestheticViewSet)
+router.register(r"moves", views.MoveViewSet)
+router.register(r"lore", views.LoreViewSet)
+
+urlpatterns = [path("", include(router))]
