41555650e293cfcd947ea82d50ddf33ab46c6b10
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 23:01:10 -0700

    registered all season models

--- wanderhome/seasons/admin.py
+++ wanderhome/seasons/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Season, Month, Lack, Sign, Event, Effect, EffectMove, Holiday, Tradition, HolidayMove, Custom
 # Register your models here.
+
+admin.site.register([Season, Month, Lack, Sign, Event,
+                     Effect, EffectMove, Holiday, Tradition, HolidayMove, Custom])
