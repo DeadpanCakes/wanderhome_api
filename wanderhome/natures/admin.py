e756bad8f43c07d91a4a9a364c9aa495ef00e150
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 21:18:52 -0700

    registered models on admin site

--- wanderhome/natures/admin.py
+++ wanderhome/natures/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register([models.Category, models.Nature,
+                    models.Aesthetic, models.Lore, models.Move])
