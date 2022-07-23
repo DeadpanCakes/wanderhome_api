ba59184c5394fabb25d73aef52c7641f4ee5dabe
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 20:10:27 -0700

    made layout of Nature model

--- wanderhome/natures/models.py
+++ wanderhome/natures/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+from django.db.models import Model, CharField, ForeignKey, CASCADE
+from options import Option
 
 # Create your models here.
+
+
+class Nature(Model):
+    name = CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Categories(Model):
+    name = CharField(max_length=20)
+    nature = ForeignKey(Nature, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Aesthetic(Option):
+    name = CharField(max_length=100)
+    nature = ForeignKey(Nature, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Move(Model):
+    prompt = CharField(max_length=300)
+    nature = ForeignKey(Nature, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.prompt
+
+
+class Lore(Option):
+    prompt = CharField(max_length=200)
+    nature = ForeignKey(Nature, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.prompt
