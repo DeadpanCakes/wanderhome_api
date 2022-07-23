fc5610c5efa3c178f7c489b18310b5c4cb171a22
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 19:55:46 -0700

    added views to router

--- wanderhome/playbooks/urls.py
+++ wanderhome/playbooks/urls.py
@@ -1,1 +1,17 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r"playbooks", views.PlaybookViewSet)
+router.register(r"animals", views.AnimalViewSet)
+router.register(r"personalities", views.PersonalityViewSet)
+router.register(r"appearances", views.AppearanceViewSet)
+router.register(r"histories", views.HistoryViewSet)
+router.register(r"relationships", views.RelationshipViewSet)
+router.register(r"signature-moves", views.SignatureMoveViewSet)
+router.register(r"seasonal-moves", views.SeasonalMoveViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
