7a1532f84fa6bee3c140f5953e91e31de1b4c19f
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-22 19:55:18 -0700

    made serializers for all other models

---
+++ wanderhome/playbooks/serializers.py
@@ -0,0 +1,75 @@
+from . import models
+from rest_framework.serializers import HyperlinkedModelSerializer
+
+
+class AnimalsSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Animal
+        fields = ["url", "name"]
+
+
+class PersonalityOptionsSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.PersonalityOption
+        fields = ["url", "name"]
+
+
+class PersonalitiesSerializer(HyperlinkedModelSerializer):
+    options_set = PersonalityOptionsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Personality
+        fields = ["url", "prompt", "options_set"]
+
+
+class AppearancesSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Appearance
+        fields = ["url", "name"]
+
+
+class HistoryOptionsSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.HistoryOption
+        fields = ["url", "name"]
+
+
+class HistoriesSerializer(HyperlinkedModelSerializer):
+    options_set = HistoryOptionsSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.History
+        fields = ["url", "prompt", "options_set"]
+
+
+class RelationshipsSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Relationship
+        fields = ["url", "prompt"]
+
+
+class SignaturueMovesSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.SignatureMove
+        fields = "__all__"
+
+
+class SeasonalMovesSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.SeasonalMove
+        fields = "__all__"
+
+
+class PlaybooksSerializer(HyperlinkedModelSerializer):
+    animal_set = AnimalsSerializer(many=True, read_only=True)
+    personality_set = PersonalitiesSerializer(many=True, read_only=True)
+    appearance_set = AppearancesSerializer(many=True, read_only=True)
+    history_set = HistoriesSerializer(many=True, read_only=True)
+    relationship_set = RelationshipsSerializer(many=True, read_only=True)
+    signature_moves_set = SignaturueMovesSerializer(many=True, read_only=True)
+    seasonal_moves_set = SeasonalMovesSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Playbook
+        fields = ["url", "name", "desc", "animal_set", "personality_set", "appearance_set",
+                  "history_set", "relationship_set", "signature_moves_set", "seasonal_moves_set"]
