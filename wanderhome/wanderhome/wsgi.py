9bda6488fbc1fa5ff01bf50c92124a49e8b4e578
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 12:43:56 -0700

    changed location of settings

--- wanderhome/wanderhome/wsgi.py
+++ wanderhome/wanderhome/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wanderhome.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wanderhome.wanderhome.settings')
 
 application = get_wsgi_application()
