792a137d86715301fd7c0b06ae718253e42d2669
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-25 12:49:36 -0700

    changed debug settings for production

--- wanderhome/wanderhome/settings.py
+++ wanderhome/wanderhome/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
 
 ALLOWED_HOSTS = []
 
