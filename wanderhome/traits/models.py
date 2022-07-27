d44c77141df23607814a27bcee576b0605d149a2
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-26 18:39:01 -0700

    fixed bug preventing trait instantiation

--- wanderhome/traits/models.py
+++ wanderhome/traits/models.py
@@ -6,22 +6,22 @@ from options.models import Option
 app_name = "traits"
 
 
-class TraitCategory(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=20, unique=True)
-    is_traumatized = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
-
-
 class Trait(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20, unique=True)
     description = models.CharField(max_length=200, unique=True)
     is_magic = models.BooleanField(default=False)
-    category = models.ForeignKey(
-        TraitCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class TraitCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20, unique=True)
+    is_traumatized = models.BooleanField(default=False)
+    traits = models.ForeignKey(
+        Trait, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
