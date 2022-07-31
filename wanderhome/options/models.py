8c853acf76a5a8eff3eeb8dedbd38f5031a07199
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-30 21:59:27 -0700

    increased length of other fields

--- wanderhome/options/models.py
+++ wanderhome/options/models.py
@@ -7,11 +7,11 @@ class Option(models.Model):
     id = models.AutoField(primary_key=True)
     text = models.CharField(max_length=500)
     non_magic_text = models.CharField(
-        max_length=100, null=True, blank=True)
+        max_length=500, null=True, blank=True)
     non_traumatized_text = models.CharField(
-        max_length=100, null=True, blank=True)
+        max_length=500, null=True, blank=True)
     non_traumatized_or_magic_text = models.CharField(
-        max_length=100, null=True, blank=True)
+        max_length=500, null=True, blank=True)
     is_magical = models.BooleanField(default=False)
     is_traumatized = models.BooleanField(default=False)
 
